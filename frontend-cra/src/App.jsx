9465a56803b1604a6b33535d336ca4d7987d0dc0
Larigit <larissa.fsandrade@ufpe.br>
2021-02-25 11:06:12 -0300

    update

---
+++ frontend-cra/src/App.jsx
@@ -0,0 +1,16 @@
+// rfc
+// jsx é a estrutura html com js
+import React from 'react'
+
+function App() {
+  // retorna html
+  return (
+    <>
+      <div>
+        Hello 
+      </div>
+    </>
+  )
+}
+
+export default App;
